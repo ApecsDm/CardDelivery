e491194190e5f94350e47ab8512de2f3f9e9c74e
ApecsDm <0133953@bk.ru>
2022-05-09 19:07:12 +0300

    CardDelivery.java modified

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -6,15 +6,21 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
     @Test
     void shouldDeliveryCheckBox() {
-        Configuration.holdBrowserOpen=true;
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
-        $("//*[@data-test-id=\"city\"]").setValue("Рязань");
+        $x("//*[@placeholder=\"Город\"]").setValue("Рязань");
+        $x("//*[@placeholder=\"Дата встречи\"]").setValue("12052022");
+        $x("//*[@name=\"name\"]").setValue("Орлов Олег");
+        $x("//*[@name=\"phone\"]").setValue("+79992840055");
+        $x("//*[@class=\"checkbox__box\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("Встреча успешно забронирована")).should(Condition.visible, Duration.ofSeconds(16));
     }
-
 }
