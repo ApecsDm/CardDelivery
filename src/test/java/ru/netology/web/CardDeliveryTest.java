d998ab217f61f0f95264c7b0b5b429acdd9dca94
ApecsDm <0133953@bk.ru>
2022-05-09 16:59:02 +0300

    CardDelivery.jar modified

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,8 +1,20 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
     @Test
+    void shouldDeliveryCheckBox() {
+        Configuration.holdBrowserOpen=true;
+        open("http://localhost:9999");
+        $("//*[@data-test-id=\"city\"]").setValue("Рязань");
+    }
+
 }
